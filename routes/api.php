b48ca09b4df858845e1682717cf4f5ab00d7f7a4
Ryan-Riaz <devryanriaz@gmail.com>
2022-01-12 00:33:25 +0600

    Api authorization task has been successful

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,24 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+
+// public routes
 Route::get('/posts', [PostController::class, 'index']);
-Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
-Route::put('/posts/{post}', [PostController::class, 'update']);
-Route::delete('/posts/{post}', [PostController::class, 'destroy']);
 Route::get('/posts/search/{title}', [PostController::class, 'searchByTitle']);
+
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+
+// protected routes
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/posts', [PostController::class, 'store']);
+    Route::put('/posts/{post}', [PostController::class, 'update']);
+    Route::delete('/posts/{post}', [PostController::class, 'destroy']);
+
+    Route::post('/logout', [UserController::class, 'logout']);
+});
